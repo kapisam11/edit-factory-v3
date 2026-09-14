7835d43710e4a3eb75eba03db6d6e88d20dcf1ef
kapasam2000-pixel <kapasam2000@gmail.com>
2026-09-14 22:18:36 +0200

    refactor: point dashboard wrapper to v3

--- 01-MAIN-CODE/web_app_v3.py
+++ 01-MAIN-CODE/web_app_v3.py
@@ -7,6 +7,6 @@ _WEB_DIR = _REPO_ROOT / "02-WEB-FILES"
 if str(_WEB_DIR) not in sys.path:
     sys.path.insert(0, str(_WEB_DIR))
 
-from app import web_app_v2 as _implementation
+from app import web_app_v3 as _implementation
 
 sys.modules[__name__] = _implementation
