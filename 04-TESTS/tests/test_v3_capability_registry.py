02555b6310b1418fa5bfb579467b995cedd79a85
kapisam11 <268908837+kapisam11@users.noreply.github.com>
2026-09-15 16:41:36 +0200

    test: enforce evidenced forty-capability registry

---
+++ 04-TESTS/tests/test_v3_capability_registry.py
@@ -0,0 +1,8 @@
+from ai_video_factory.v3_capabilities import CAPABILITIES, validate_capabilities
+
+
+def test_v3_capability_registry_is_complete_and_evidenced():
+    validate_capabilities()
+    assert len(CAPABILITIES) == 40
+    assert all(spec.status == "implemented" for spec in CAPABILITIES.values())
+    assert all(spec.implementation and spec.validator and spec.tests for spec in CAPABILITIES.values())
